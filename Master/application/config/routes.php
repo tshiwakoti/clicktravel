9eb9eec9cf9f64c581262c5a79f40c3dc9146cc1
tshiwakoti <tshiwakoti@outlook.com>
2015-07-19 17:48:33 -0700

    Clean up the master folder

--- Master/application/config/routes.php
+++ Master/application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'CourseController';
+$route['default_controller'] = '';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['/Productscontroller/add'] ='addproduct';
+//$route['/Productscontroller/add'] ='addproduct';
--- Master/application/views/index.php
+++ Master/application/views/index.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Title</title>
+		<title>Ecommerce Site</title>
 
 		<style>
 
